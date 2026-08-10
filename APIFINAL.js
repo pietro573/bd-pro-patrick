cdf5d12c5cda6ad9026cbd8528afc1ee356f6b01
crazy programmer <pietro.czerski@escola.pr.gov.br>
2026-08-10 12:19:09 -0300

    Update APIFINAL.js

--- APIFINAL.js
+++ APIFINAL.js
@@ -2,3 +2,20 @@ const express = require ("express")
 const app = express()
 const port = 3000
 app.use(express.json())
+
+separar depois
+
+// npm i mysql2
+const mysql = require("mysql2/promise")
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: 3306,
+    user:'root',
+    password: 'escola',
+    database: 'aula'
+})
+
+module.exports = object.freeze({
+    pool: pool
+})
